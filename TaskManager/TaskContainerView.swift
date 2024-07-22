5f62ed7f747803312e04267b3b2b926f43a377f2
Matthew O'Donnell <matthew.james.od@gmail.com>
2024-07-22 07:45:48 +0100

    moved binary search algo to utils

--- TaskManager/TaskContainerView.swift
+++ TaskManager/TaskContainerView.swift
@@ -1,26 +1,7 @@
 import SwiftUI
+import UtilAlgorithms
 import KillerModels
 import KillerData
-
-extension RandomAccessCollection {
-    /// Finds such index N that predicate is true for all elements up to
-    /// but not including the index N, and is false for all elements
-    /// starting with index N.
-    /// Behavior is undefined if there is no such N.
-    func binarySearch(predicate: (Element) -> Bool) -> Index {
-        var low = startIndex
-        var high = endIndex
-        while low != high {
-            let mid = index(low, offsetBy: distance(from: low, to: high)/2)
-            if predicate(self[mid]) {
-                low = index(after: mid)
-            } else {
-                high = mid
-            }
-        }
-        return low
-    }
-}
 
 // TODO: Mutating observable property \TaskListViewModel.tasks after view is torn down has no effect
 
