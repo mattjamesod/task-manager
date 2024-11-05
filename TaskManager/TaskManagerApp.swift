fca2ab68021f49e95a385e2cdee2b6fe199f42ef
Matthew O'Donnell <matthew.james.od@gmail.com>
2024-11-05 08:45:27 +0000

    added rough shake to undo support

--- TaskManager/TaskManagerApp.swift
+++ TaskManager/TaskManagerApp.swift
@@ -43,6 +43,8 @@ struct ScopeNavigationWindow: Scene {
     @State var canUndo: Bool = false
     @State var canRedo: Bool = false
     
+    @State var shakeUndoConfirm: Bool = false
+    
     let database: Database?
     
     var body: some Scene {
@@ -70,6 +72,18 @@ struct ScopeNavigationWindow: Scene {
                         }
                         .environment(\.canUndo, self.canUndo)
                         .environment(\.canRedo, self.canRedo)
+                        .onShake {
+                            guard canUndo else { return }
+                            shakeUndoConfirm = true
+                        }
+                        .alert("Undo?", isPresented: $shakeUndoConfirm) {
+                            Button("Undo!") {
+                                Task.detached {
+                                    await database.undo()
+                                }
+                            }
+                            Button("Cancel", role: .cancel) {}
+                        }
                 }
                 else {
                     CatastrophicErrorView()
@@ -193,3 +207,40 @@ extension View {
             .modifier(MainWindowContentViewModifier())
     }
 }
+
+
+#if canImport(UIKit)
+// The notification we'll send when a shake gesture happens.
+extension UIDevice {
+    static let deviceDidShakeNotification = Notification.Name(rawValue: "deviceDidShakeNotification")
+}
+
+//  Override the default behavior of shake gestures to send our notification instead.
+extension UIWindow {
+     open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            NotificationCenter.default.post(name: UIDevice.deviceDidShakeNotification, object: nil)
+        }
+     }
+}
+
+// A view modifier that detects shaking and calls a function of our choosing.
+struct DeviceShakeViewModifier: ViewModifier {
+    let action: () -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.deviceDidShakeNotification)) { _ in
+                action()
+            }
+    }
+}
+
+// A View extension to make the modifier easier to use.
+extension View {
+    func onShake(perform action: @escaping () -> Void) -> some View {
+        self.modifier(DeviceShakeViewModifier(action: action))
+    }
+}
+#endif
