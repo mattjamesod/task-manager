28d8da804d68b794b6028f8bd6edbb9542a50fca
Matthew O'Donnell <matthew.james.od@gmail.com>
2024-11-30 11:41:10 +0000

    removed unused logging

--- Packages/KillerData/Sources/KillerData/AsyncMessageHandler.swift
+++ Packages/KillerData/Sources/KillerData/AsyncMessageHandler.swift
@@ -10,7 +10,7 @@ public enum DatabaseMessage: Sendable {
     case recordDeleted(id: Int)
 }
 
-public actor AsyncMessageHandler<T: Sendable>: CustomConsoleLogger {
+public actor AsyncMessageHandler<T: Sendable> {
     public struct Thread: Identifiable, Sendable {
         nonisolated public let id: UUID = .init()
         public var events: AsyncChannel<T>
@@ -22,26 +22,20 @@ public actor AsyncMessageHandler<T: Sendable>: CustomConsoleLogger {
     
     public init() { }
     
-    // TODO: make this logging useful when this class has many use cases
-    nonisolated public let logToConsole: Bool = false
-    
     private var messageThreads: [Thread] = []
     
     public func subscribe() -> Thread {
         let newThread = Thread()
         self.messageThreads.append(newThread)
-        self.log("New subscription, total: \(self.messageThreads.count)")
         return newThread
     }
     
     public func unsubscribe(_ thread: Thread) {
         guard let index = self.messageThreads.firstIndex(where: { $0.id == thread.id }) else { return }
         self.messageThreads.remove(at: index)
-        self.log("Removed subscription, total: \(self.messageThreads.count)")
     }
     
     public func send(_ message: T) async {
-        self.log("Sending message to \(self.messageThreads.count) listeners: \(message)")
         for thread in messageThreads {
             await thread.events.send(message)
         }
